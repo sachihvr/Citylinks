bdd65500b477d4f94a453bfca93afaa98819a439
sachihvr <sachi.hvr@gmail.com>
2026-02-28 16:56:05 +0530

    Fix: Aligned app registry name and bundled vector icons

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
+// Registering 'main' satisfies the Android Native ignition
+AppRegistry.registerComponent('main', () => App);
+
+// Registering appName satisfies standard React Native
 AppRegistry.registerComponent(appName, () => App);
